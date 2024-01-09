f54b531058f9582cac155cada279f14fabc23721
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-01-10 04:49:34 +0530

    worked on navigation

---
+++ app/src/main/java/com/example/navigationkotlin/FirstScreen.kt
@@ -0,0 +1,23 @@
+package com.example.navigationkotlin
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FirstScreen(nextScreen: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { nextScreen() }) {
+            Text(text = "Go To Next Screen")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/navigationkotlin/SecondScreen.kt
@@ -0,0 +1,24 @@
+package com.example.navigationkotlin
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SecondScreen(previousScreen: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FilledTonalButton(onClick = { previousScreen() }) {
+            Text(text = "Go To Previous Screen")
+        }
+    }
+}
